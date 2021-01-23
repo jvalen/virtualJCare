c67058fff52828ffc10a54e44c9820ef2979e25e
jvalen <javiervalenciaromero@gmail.com>
2021-01-23 18:55:26 +0100

    Remove unnecessary files

--- src/main.js
+++ src/main.js
@@ -28,51 +28,8 @@ class Game extends Phaser.Game {
     this.state.add("MainMenu", MainMenuState, false);
     this.state.add("Game", GameState, false);
 
-    // with Cordova with need to wait that the device is ready so we will call the Boot state in another file
-    if (!window.cordova) {
-      this.state.start("Boot");
-    }
+    this.state.start("Boot");
   }
 }
 
 window.game = new Game();
-
-// if (window.cordova) {
-//   var app = {
-//     initialize: function () {
-//       document.addEventListener(
-//         "deviceready",
-//         this.onDeviceReady.bind(this),
-//         false
-//       );
-//     },
-
-//     // deviceready Event Handler
-//     //
-//     onDeviceReady: function () {
-//       this.receivedEvent("deviceready");
-
-//       // When the device is ready, start Phaser Boot state.
-//       window.game.state.start("Boot");
-//     },
-
-//     receivedEvent: function (id) {
-//       console.log("Received Event: " + id);
-//     },
-//   };
-
-//   app.initialize();
-// }
-
-// if ("serviceWorker" in navigator) {
-//   window.addEventListener("load", () => {
-//     navigator.serviceWorker
-//       .register("/service-worker.js")
-//       .then((registration) => {
-//         console.log("SW registered: ", registration);
-//       })
-//       .catch((registrationError) => {
-//         console.log("SW registration failed: ", registrationError);
-//       });
-//   });
-// }
